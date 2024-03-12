6ccd22bb0556531bd9d1e91a591aaa8c59c56e05
Jairo <jairoregiscavalcanti@gmail.com>
2024-03-12 17:04:27 -0300

    Pratico_2_Estrutura_try_catch_atualizado_#1

--- Seção15/Estrutura_try_catch/Pratico2/Principal.java
+++ Seção15/Estrutura_try_catch/Pratico2/Principal.java
@@ -1,6 +1,6 @@
 package Seção15.Estrutura_try_catch.Pratico2;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -13,37 +13,66 @@ public class Principal {
      
       while (!t) {
         
-      System.out.println("Insira a operação que deseja efetuar: 1 - Definir tamanho do vetor // 2 - Visualizar vetor");
+      System.out.println("Insira a operação que deseja efetuar: 1 - Definir tamanho do vetor // 2 - Visualizar vetor // 3 - Inserir elementos no vetor");
       String opcao = scanner.next();
   
       switch (opcao) {
         
-        case "1":
+      case "1":
         
+      if(vector.length == 0){
+
         try {
+          System.out.println("----");
           System.out.println("Insira o tamanho do array que deseja criar: ");
           int num_1 = scanner.nextInt(); 
           vector = new int[num_1];
-          System.out.println(Arrays.toString(vector));
+          System.out.println("Vetor de tamanho " +  num_1 + " criado!");
+          System.out.println("----");
+          // System.out.println(Arrays.toString(vector));
 
         }catch (InputMismatchException e) {
+          System.out.println("----");
           System.out.println("!! Apenas entradas numéricas inteiras são permitidas na criação de tamanho dos vetores !!");    
           scanner.nextLine(); // Limpa o buffer do scanner
+          System.out.println("----");
           // usamos scanner.nextLine() para limpar o buffer do Scanner após capturar uma exceção de entrada incorreta
         }
-
-        break;
       
-        case "2":
+        }else{
+          System.out.println("Vetor ja foi criado!");
+        }
+       
+      break;
+      
+
+      case "2":
+
+      if(vector.length !=0){
+        System.out.println("----------------------------");
+        System.out.println("-- ARRAY --");
+          for (int i : vector) {
+            System.out.printf("[" + i + "]");
+            }
+        System.out.println("\n----------------------------");
+        }else{
+          System.out.println("Vetor ainda nao foi preenchido ! \n");
+        }
+        
+      break;
+
+      case "3":
+      
+      
+
+      break;
 
 
-        break;
-
-        default:
+      default:
           
         System.out.println("Insira uma mensagem valida");
         
-        break;
+      break;
         
         
         }
