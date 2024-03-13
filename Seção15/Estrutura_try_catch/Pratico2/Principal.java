cf673f541f6cc125555510e2794f897fae78c6f1
Jairo <jairoregiscavalcanti@gmail.com>
2024-03-13 18:24:13 -0300

    Pratico_2_estrutura_try_catch_inserido

--- Seção15/Estrutura_try_catch/Pratico2/Principal.java
+++ Seção15/Estrutura_try_catch/Pratico2/Principal.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class Principal { 
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
-      int vector[] = new int[0];
+      String vector[] = new String[0];
+      boolean t_2 = false;
       boolean t = false;
      
       while (!t) {
@@ -26,7 +27,7 @@ public class Principal {
           System.out.println("----");
           System.out.println("Insira o tamanho do array que deseja criar: ");
           int num_1 = scanner.nextInt(); 
-          vector = new int[num_1];
+          vector = new String[num_1];
           System.out.println("Vetor de tamanho " +  num_1 + " criado!");
           System.out.println("----");
           // System.out.println(Arrays.toString(vector));
@@ -48,29 +49,43 @@ public class Principal {
 
       case "2":
 
-      if(vector.length !=0){
+      if(vector.length != 0){
         System.out.println("----------------------------");
         System.out.println("-- ARRAY --");
-          for (int i : vector) {
+          for (String i : vector) {
             System.out.printf("[" + i + "]");
             }
         System.out.println("\n----------------------------");
         }else{
-          System.out.println("Vetor ainda nao foi preenchido ! \n");
+          System.out.println(" -- Impossivel visualizar: Vetor ainda nao foi criado ! -- \n");
         }
         
       break;
 
       case "3":
-      
-      
+      if(vector.length == 0){
+        System.out.println(" -- Impossivel preencher: Vetor ainda nao foi criado ! -- \n");
+      }
+
+      for(int i = 0; i< vector.length; i++){
+       
+        if(t_2 != true){
+          System.out.println("----------------------------");
+          System.out.println("Insira o elemento de posição #" + i + " do vetor: ");
+          vector[i] = scanner.next();
+          System.out.println("----------------------------");
+        }else{
+          System.out.println(" -- Vetor já foi preenchido! -- ");
+        }
+        t_2 = true;
+      }
+
 
       break;
 
-
       default:
           
-        System.out.println("Insira uma mensagem valida");
+        System.out.println("Insira uma mensagem válida");
         
       break;
         
